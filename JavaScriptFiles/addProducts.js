ab8bcf1ee138ce0882eb0c73db588524f37419b5
Kübra Nur GÜRSOY <kubranurgursoyy@hotmail.com>
2022-05-29 23:48:55 +0300

    cors error

--- JavaScriptFiles/addProducts.js
+++ JavaScriptFiles/addProducts.js
@@ -37,7 +37,7 @@ async function postProductsApi(url) {
         body: _wdata,
         headers: {
             "Content-type": "application/json; charset=UTF-8",
-            "Access-Control-Allow-Origin": "http://warehouse-report-app-backend.herokuapp.com/api/products/"
+            "Access-Control-Allow-Origin": "https://warehouse-report-app-backend.herokuapp.com/api/products/"
         }}
     const response = await fetch(url,options)
 }
--- JavaScriptFiles/makeReport.js
+++ JavaScriptFiles/makeReport.js
@@ -1,5 +1,5 @@
 const api_url =
-    "http://warehouse-report-app-backend.herokuapp.com/api/report";
+    "https://warehouse-report-app-backend.herokuapp.com/api/report";
 
 
 async function getapi() {
