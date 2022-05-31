21bf300e237a0b5989136a8e9e1182ab655c63de
KubraNurGursoyyy <kubranurgursoyy@hotmail.com>
2022-05-31 15:15:28 +0300

    When changing Vat rate automaticlly change grey fields

--- JavaScriptFiles/VatRateCalculate.js
+++ JavaScriptFiles/VatRateCalculate.js
@@ -4,13 +4,35 @@ function getVatRate(){
     console.log("VatRate:", $("#vatRate")[0].value);
     vatRate = $("#vatRate")[0].value;
     console.log("Vat Rate Value" , vatRate)
+    console.log("Formdaki inputlar?", document.getElementById('productForm').getElementsByTagName('input'));
+    var inputs = document.getElementById('productForm').getElementsByTagName('input');
+    console.log("Inputs:", inputs);
+    for(let i = 0; i <= 6; i++){
+        console.log("Inputs i :", inputs[i], inputs[i].id, i);
+
+        if(inputs[i].style.background.includes("rgb(227, 227, 227) none repeat scroll 0% 0%")){
+
+            console.log("Inputs i :", inputs[i], inputs[i].id, i);
+
+            if(inputs[i].id.includes("WithVat")){
+                var inputPrice = inputs[i - 1].value;
+                document.getElementById(inputs[i].id).value = findPriceWithVatRateFromPrice(inputPrice);
+            }
+            else{
+                var vatPrice = inputs[i + 1].value;
+                document.getElementById(inputs[i].id).value = findPriceFromPriceWithVatRate(vatPrice);
+
+            }
+        }
+
+    }
 }
 
 function changeInPrice(event){
     var price = document.getElementById(event).value;
     console.log(event+"WithVat");
     document.getElementById(event).style.background = 'rgb(255, 255, 255)';
-    document.getElementById(event+"WithVat").style.background = 'rgba(227,227,227,255)';
+    document.getElementById(event+"WithVat").style.background = 'rgba(227,227,227)';
     document.getElementById(event+"WithVat").value = findPriceWithVatRateFromPrice(price);
 
 }
